9d88b3d874f11ecdde5276d467d294b24ef9c0d1
Seungwon Park <psw14041@gmail.com>
2016-10-24 14:33:08 +0900

    Resolved #2

--- UTCtoJD.php
+++ UTCtoJD.php
@@ -1,5 +1,6 @@
 <?php
-	// Refer to http://aa.usno.navy.mil/faq/docs/JD_Formula.php
+	// // Refer to http://aa.usno.navy.mil/faq/docs/JD_Formula.php
+	// Formula given above seems to be wrong. Refer to https://en.wikipedia.org/wiki/Julian_day
 	function sign($x){
 		if($x>0) return 1;
 		else return -1;
@@ -12,6 +13,9 @@
 		}
 	}
 	function UTCtoJD($year, $month, $day, $hour, $minute, $second){
-		return 367 * $year - truncate( ( 7 * ($year + truncate(($month + 9)/12) ) ) / 4 ) + truncate(275 * $minute / 9) + $day + 1721013.5 + $hour / 24 + $minute / 1440 + $second / 86400;
+		$temp_a = intval((14-$month)/12);
+		$temp_y = $year + 4800 - $temp_a;
+		$temp_m = $month + 12*$temp_a - 3;
+		return 365*$temp_y + intval(($temp_y/4)) - intval(($temp_y/100)) + intval(($temp_y/400)) + intval((153*$temp_m + 2)/5) + $day - 32045;
 	}
 ?>
